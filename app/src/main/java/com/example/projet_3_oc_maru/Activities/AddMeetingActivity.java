51d82777d109c8790412f1fb110de2a5ae97d663
Azubal <valentinfamery087@gmail.com>
2021-02-14 22:03:46 +0100

    Commit 4.0 add one  timepicker about two

--- app/src/main/java/com/example/projet_3_oc_maru/Activities/AddMeetingActivity.java
+++ app/src/main/java/com/example/projet_3_oc_maru/Activities/AddMeetingActivity.java
@@ -2,30 +2,41 @@ package com.example.projet_3_oc_maru.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 import com.example.projet_3_oc_maru.DI.DI;
 import com.example.projet_3_oc_maru.Fragments.MainFragment;
 import com.example.projet_3_oc_maru.Models.Meeting;
 import com.example.projet_3_oc_maru.Models.RoomMeeting;
 import com.example.projet_3_oc_maru.R;
-import com.example.projet_3_oc_maru.events.DeleteMeetingEvent;
 import com.google.android.material.textfield.TextInputLayout;
 
-import org.greenrobot.eventbus.EventBus;
 
+import java.util.Calendar;
 import java.util.Objects;
 
-public class AddMeetingActivity extends AppCompatActivity {
+public class AddMeetingActivity extends AppCompatActivity  {
+
     TextInputLayout idMeeting;
     TextInputLayout subjectMeeting;
     TextInputLayout timeBegin;
     TextInputLayout timeEnd;
     TextInputLayout participantsMeeting;
-    private boolean statutForChange = true;
+    EditText timeBeginMeeting;
+    Button btnTimePicker;
+
+    private int  mHour, mMinute;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +48,19 @@ public class AddMeetingActivity extends AppCompatActivity {
         timeEnd = findViewById(R.id.timeEndLyt);
         participantsMeeting = findViewById(R.id.participantsMeetingLyt);
         Button createNewRoomMeetingButton = findViewById(R.id.create);
-        NumberPicker numberRoomMeeting = (NumberPicker) findViewById(R.id.numberRoomMeeting);
 
+
+         btnTimePicker= findViewById(R.id.btn_time_begin);
+        timeBeginMeeting = findViewById(R.id.timeBeginMeeting);
+
+
+
+        NumberPicker numberRoomMeeting = findViewById(R.id.numberRoomMeeting);
         numberRoomMeeting.setMinValue(1);
         numberRoomMeeting.setMaxValue(10);
         numberRoomMeeting.setWrapSelectorWheel(true);
-
-
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
 
         createNewRoomMeetingButton.setOnClickListener(v -> {
             Meeting meeting = new Meeting(
@@ -56,13 +72,41 @@ public class AddMeetingActivity extends AppCompatActivity {
                     RoomMeeting.getRoomMeetingById(numberRoomMeeting.getValue())
             );
             DI.getMeetingApiService().createMeeting(meeting);
-
             finish();
+        });
 
+        btnTimePicker.setOnClickListener(v -> {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    (view, hourOfDay, minute) -> {
+
+                timeBeginMeeting.setText(hourOfDay + ":" + minute);
+
+
+
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
 
         });
 
+
+
+
+
+
+
+
+
     }
+
+
+
 
 
 
