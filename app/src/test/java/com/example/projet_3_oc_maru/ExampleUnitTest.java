8b11d6ba9ec9aab93394730577397e5db30f5006
Azubal <valentinfamery087@gmail.com>
2021-03-04 14:42:49 +0100

    Commit 8.5 Add responsive Layout for all App with constraint Layout

--- app/src/test/java/com/example/projet_3_oc_maru/ExampleUnitTest.java
+++ app/src/test/java/com/example/projet_3_oc_maru/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.example.projet_3_oc_maru;
 
+import com.example.projet_3_oc_maru.di.DI;
+import com.example.projet_3_oc_maru.models.Meeting;
+import com.example.projet_3_oc_maru.service.DummyMeetingGenerator;
+import com.example.projet_3_oc_maru.service.MeetingApiService;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +18,33 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    private MeetingApiService service;
+
+    @Before
+    public void setup() {
+        service = DI.getNewInstanceApiService();
     }
+
+    @Test
+    public void getMeetingsWithSuccess() {
+        List<Meeting> meetings = service.getMeetings();
+        List<Meeting> expectedMeetings  = DummyMeetingGenerator.DUMMY_ROOM_MEETS;
+        assertEquals(meetings,expectedMeetings);
+    }
+
+    @Test
+    public void addMeetingWithSuccess() {
+        Meeting meetingToAdd = DummyMeetingGenerator.DUMMY_ROOM_MEETS.get(0);
+        service.createMeeting(meetingToAdd);
+        assertTrue(service.getMeetings().contains(meetingToAdd));
+    }
+
+    @Test
+    public void deleteMeetingWithSuccess() {
+        Meeting meetingToDelete = service.getMeetings().get(0);
+        service.deleteMeeting(meetingToDelete);
+        assertFalse(service.getMeetings().contains(meetingToDelete));
+    }
+
 }
