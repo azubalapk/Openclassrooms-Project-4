045f4b9c577520905b6f6d334f95638cf9f63b9d
Azubal <valentinfamery087@gmail.com>
2021-03-20 18:02:31 +0100

    Commit 13.4 Save Code

--- app/src/androidTest/java/com/example/projet_3_oc_maru/InstrumentedTest.java
+++ app/src/androidTest/java/com/example/projet_3_oc_maru/InstrumentedTest.java
@@ -1,21 +1,37 @@
 package com.example.projet_3_oc_maru;
-import androidx.recyclerview.widget.RecyclerView;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.example.projet_3_oc_maru.activities.MainActivity;
-import com.example.projet_3_oc_maru.di.DI;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
+import static org.hamcrest.EasyMock2Matchers.equalTo;
+import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -37,8 +53,34 @@ public class InstrumentedTest {
     @Test
     public void deleteMeeting() {
 
+    
     }
+    @Test
+    public void addMeeting() {
 
+    }
+    @Test
+    public void filterRoomIsWorking() {
+    }
+    @Test
+    public void filterDateIsWorking() {
+    }
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
 
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
 
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
