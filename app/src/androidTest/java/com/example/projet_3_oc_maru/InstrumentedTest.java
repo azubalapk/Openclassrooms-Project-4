1a315a34635b65393e685f19a8bf831c5e8edfb7
valen <valentinfamery087@gmail.com>
2021-03-24 18:46:06 +0100

    Commit 13.6 Add test filter

--- app/src/androidTest/java/com/example/projet_3_oc_maru/InstrumentedTest.java
+++ app/src/androidTest/java/com/example/projet_3_oc_maru/InstrumentedTest.java
@@ -8,14 +8,16 @@ import android.view.ViewParent;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.example.projet_3_oc_maru.activities.AddActivity;
 import com.example.projet_3_oc_maru.activities.MainActivity;
+import com.example.projet_3_oc_maru.di.DI;
+import com.example.projet_3_oc_maru.models.Meeting;
+import com.example.projet_3_oc_maru.service.MeetingApiService;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -25,13 +27,11 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.getIdlingResources;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
@@ -40,30 +40,26 @@ import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.array;
-import static org.hamcrest.Matchers.arrayContaining;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 
 
 @RunWith(AndroidJUnit4.class)
 public class InstrumentedTest {
+    MeetingApiService apiService;
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    public ActivityScenarioRule activityRuleAdd = new ActivityScenarioRule<>(AddActivity.class);
-
-
     @Before
     public void setUp() {
          activityRule.getScenario();
+         //apiService = DI.getMeetingApiService();
     }
 
     @Test
     public void listMeetingContainsAllMeetingInitial() {
-        onView(withId(R.id.list_meetings)).check(matches(hasChildCount(3)));
+        onView(withId(R.id.list_meetings)).check(matches(hasChildCount(apiService.getMeetings().size())));
     }
 
     @Test
@@ -100,40 +96,31 @@ public class InstrumentedTest {
         onView(withId(R.id.addParticipant)).perform(click()).check(matches(isDisplayed()));
         onView(withId(R.id.chipGroup)).check(matches(hasChildCount(2)));
         onView(withId(R.id.create)).perform(click());
-        onView(withId(R.id.list_meetings)).check(matches(hasChildCount(4)));
-
-
-
-
+        onView(withId(R.id.list_meetings)).check(matches(hasChildCount(apiService.getMeetings().size()+1)));
 
     }
     @Test
-    public void filterRoomIsWorking() {
-
+    public void filterRoomIsWorking()  {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Filtrer")).perform(click());
+        onView(withText("Sélectionner salle")).perform(click());
+        onView(withText("Mario")).perform(click());
+        onView(ViewMatchers.withId(R.id.list_meetings)).check(matches(hasChildCount(1)));
     }
     @Test
     public void filterDateIsWorking() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Filtrer")).perform(click());
+        onView(withText("Sélectionner date")).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021,2,14));
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(ViewMatchers.withId(R.id.list_meetings)).check(matches(hasChildCount(1)));
+    }
+    @Test
+    public void filterAllMeetingIsWorking() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Toutes les réunions")).perform(click());
+        onView(ViewMatchers.withId(R.id.list_meetings)).check(matches(hasChildCount(apiService.getMeetings().size())));
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position "
-                        + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup
-                        && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent)
-                        .getChildAt(position));
-            }
-        };
-    }
 }
