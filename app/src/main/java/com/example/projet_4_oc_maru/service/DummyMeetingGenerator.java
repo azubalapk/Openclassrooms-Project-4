e2e2782622c627823e1fa96c64810d0690292673
Azubal <valentinfamery087@gmail.com>
2021-04-20 23:24:58 +0200

    Commit 17.0 Modify strings.xml

--- app/src/main/java/com/example/projet_4_oc_maru/service/DummyMeetingGenerator.java
+++ app/src/main/java/com/example/projet_4_oc_maru/service/DummyMeetingGenerator.java
@@ -2,9 +2,7 @@ package com.example.projet_4_oc_maru.service;
 
 import com.example.projet_4_oc_maru.models.Meeting;
 import com.example.projet_4_oc_maru.models.RoomMeeting;
-
 import org.joda.time.DateTime;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
